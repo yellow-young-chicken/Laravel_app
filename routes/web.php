947d28402c9980e9aa4288542b66a0654962a2f6
yellow-young-chicken <114807506+yellow-young-chicken@users.noreply.github.com>
2023-02-21 14:35:27 +0000

    2/21

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,9 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+  Route::resource('posts', 'PostController');
+ 
+  if (env('APP_ENV') === 'local') {
+     URL::forceScheme('https');
+  } 
