27a07902a043d47decc415ebb7120b912b2f0b65
yellow-young-chicken <114807506+yellow-young-chicken@users.noreply.github.com>
2023-02-23 18:47:37 +0000

    index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,1 +1,32 @@
-<h1>Posts</h1> 
+<!DOCTYPE html>
+ <html lang="ja">
+ 
+ <head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>投稿一覧</title>
+ </head>
+ 
+ <body>
+     <header>
+         <nav>
+             <div>
+                 <a href="#">投稿アプリ</a>            
+             </div>
+         </nav>
+     </header>
+     
+     <main>
+         <article>
+             <div>                
+                 <h1>投稿一覧</h1>               
+             </div>
+         </article>
+     </main>
+ 
+     <footer>        
+         <p>&copy; 投稿アプリ All rights reserved.</p>
+     </footer>
+ </body>
+ 
+ </html>
